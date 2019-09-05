7f47c264c7c85956b5d14381086b75064d9370cc
oneNutW0nder <scb5436@rit.edu>
2019-09-04 23:05:08 -0400

    Act2 done

---
+++ hmwk_2/act2.py
@@ -0,0 +1,25 @@
+#/usr/bin/env python
+
+#
+# Name: Simon Buchheit
+# Email: scb5436@rit.edu
+#
+# File: act1.py
+#
+# Purpose: This is my implementation for act_2 of homework 2 for CSEC-380
+#
+
+import simplerequest
+
+r = simplerequest.SimpleRequest('csec380-core.csec.rit.edu', port=82, type='POST', resource='/getSecure')
+r.render()
+r.send()
+
+token = simplerequest.parse_value(r.data, "Token is:")
+
+r = simplerequest.SimpleRequest('csec380-core.csec.rit.edu', port=82, type='POST', resource='/getFlag2', body=f'token={token}')
+r.render()
+r.send()
+
+print(r.data)
+
