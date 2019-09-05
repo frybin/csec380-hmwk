3df6e7842feb396db6c14f10308abd200be1f957
oneNutW0nder <scb5436@rit.edu>
2019-09-04 22:48:56 -0400

    act1 works

---
+++ hmwk_2/act1.py
@@ -0,0 +1,18 @@
+#/usr/bin/env python
+
+#
+# Name: Simon Buchheit
+# Email: scb5436@rit.edu
+#
+# File: act1.py
+#
+# Purpose: This is my implementation for act_1 of homework 2 for CSEC-380
+#
+
+import simplerequest
+
+r = simplerequest.SimpleRequest('csec380-core.csec.rit.edu', port=82, type='POST')
+r.render()
+r.send()
+
+print(r.data)
