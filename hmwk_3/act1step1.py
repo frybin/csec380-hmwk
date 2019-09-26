610e3e9351aa56f2e1498093cad1e5721ffc37ec
oneNutW0nder <scb5436@rit.edu>
2019-09-26 00:40:32 -0400

    Docs/comments and black/flake8 complient

--- hmwk_3/act1step1.py
+++ hmwk_3/act1step1.py
@@ -4,10 +4,14 @@
 # Name: Simon Buchheit
 # Date: October 6, 2019
 #
-# Purpose: This script scrapes the site https://www.rit.edu/study/computing-security-bs 
-#           and collects the course number and corresponding course name. If there
-#           is no name with the number or vice versa the data will not be recorded.
-#           This script will output a CSV file with the data
+# CSEC-380 : hmwk3 : Act1 Part 1
+#
+# Purpose: This script scrapes the site:
+#               https://www.rit.edu/study/computing-security-bs
+#
+#           and collects the course number and corresponding course name.
+#           If there is no name with the number or vice versa the data will
+#           not be recorded. This script will output a CSV file with the data
 #
 
 import bs4
@@ -18,26 +22,26 @@ def get_values(chonker):
     """
     This function parses the initial bs4 filter for the course numbers
     and the corresponding course names. If a course does not have a number
-    or name it will not be saved. 
-    
+    or name it will not be saved.
+
     :param chonker: The massive list from the intial filter from bs4
-    :return: Returns a dictionary of course numbers and their 
+    :return: Returns a dictionary of course numbers and their
     corresponding name
     """
 
     # Dict to hold the values
     values = {}
-    
-    # List to hold the course numbers 
+
+    # List to hold the course numbers
     courseNums = []
 
     # Loop through the tags
     for tag in chonker:
         # Check for None stuff
-        if (tag.contents[1].contents[0] is not None):
+        if tag.contents[1].contents[0] is not None:
 
             # Check for newlines
-            if (tag.contents[1].contents[0] != "\xa0"):
+            if tag.contents[1].contents[0] != "\xa0":
 
                 # Make list to get just course nums
                 # Course nums don't have spaces in name
@@ -49,33 +53,41 @@ def get_values(chonker):
                     courseNums = pos_course[0]
                     # Save corresponding course name
                     # find "div" tags where "class=course-name"
-                    name = tag.contents[3].find_all("div", "course-name")[0].get_text().strip()
+                    name = (
+                        tag.contents[3]
+                        .find_all("div", "course-name")[0]
+                        .get_text()
+                        .strip()
+                    )
 
                     # Build dict
                     values[str(courseNums)] = str(name)
-    
+
     return values
-    
+
 
 def write_csv(values):
     """
     This function simply creates a new file and writes the values dict
     to the new file in a CSV format
-    
+
     :param values: Dictionary of courseNum:courseName
     """
-    
+
     with open("./courses.csv", "w+") as fd:
         for key in values:
             fd.write(f"{key},{values[key]}\n")
 
 
-
-
 def main():
 
     # Send that sweet sweet request to get the goodies
-    r = simplerequest.SimpleRequest("www.rit.edu", port=443, resource="/study/computing-security-bs", https=True)
+    r = simplerequest.SimpleRequest(
+        "www.rit.edu",
+        port=443,
+        resource="/study/computing-security-bs",
+        https=True,
+    )
     r.render()
     r.send()
 
@@ -86,8 +98,8 @@ def main():
     chonker = []
 
     # Find all "<tr>" tags with "class=hidden-row*"
-    chonker = soup.find_all('tr', "hidden-row")
-        
+    chonker = soup.find_all("tr", "hidden-row")
+
     # parse the chonker for courseNumbers
     values = get_values(chonker)
 
@@ -95,5 +107,5 @@ def main():
     write_csv(values)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
