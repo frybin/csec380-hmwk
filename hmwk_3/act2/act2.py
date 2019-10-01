95fb7ce1716a5c80936985e01b99fce441ba428b
oneNutW0nder <scb5436@rit.edu>
2019-09-30 23:14:11 -0400

    Skeleton outline done for now

--- hmwk_3/act2/act2.py
+++ hmwk_3/act2/act2.py
@@ -17,7 +17,24 @@ import os
 
 
 def main():
-    pass
+    # Sweet sweet initial req
+    req = simplerequest.SimpleRequest("www.rit.edu", port=443, https=True)
+    req.render()
+    req.send()
+
+    # Start the soup!
+    soup = bs4.BeautifulSoup(req.data["body"], "html.parser")
+    tags = soup.find_all("a")
+
+    href = []
+    for tag in tags:
+        try:
+            href.append(tag["href"])
+        except KeyError:
+            pass
+
+    # pass the list of links and host to crawler function
+    simplerequest.crawl(req.host, href)
 
 
 if __name__ == "__main__":
