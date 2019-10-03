fac420a932c93d15ada18b731ab0fa5b105a5be5
oneNutW0nder <scb5436@rit.edu>
2019-10-02 22:46:31 -0400

    broken af

--- hmwk_3/act2/simplerequest.py
+++ hmwk_3/act2/simplerequest.py
@@ -13,6 +13,11 @@
 import socket
 import ssl
 import threading
+import bs4
+import re
+import queue
+import select
+import os
 from queue import Queue
 
 # Dict of URL encodings for conversions
@@ -105,29 +110,57 @@ class SimpleRequest:
         # Create normal socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+        if self.https is False:
+            self.port = 80
+
         # Wrap sock in TLS if HTTPS is true
         if self.https:
             self.sock = ssl.wrap_socket(self.sock, ssl_version=ssl.PROTOCOL_TLS)
 
-        self.sock.connect((self.host, self.port))
+        # self.sock.setblocking(0)
+        self.sock.settimeout(10)
+        try:
+            self.sock.connect((self.host, self.port))
+        except (socket.timeout, socket.gaierror, socket.error):
+            self.sock.close()
+            self.data = None
+            return self.data
+
         self.sock.sendall(self.request.encode("utf-8"))
 
         # Shenanigans to make sure we get all data from the socket
         # because python sockets are dumb... web is dumb... reeeee
         data = b""
-        data_block = self.sock.recv(4096)
-        while data_block != b"":
-            data += data_block
+        ready = select.select([self.sock], [], [], 10)
+        if ready[0]:
             data_block = self.sock.recv(4096)
+            while data_block != b"":
+                data += data_block
+
+                ready = select.select([self.sock], [], [], 10)
+                if ready[0]:
+                    data_block = self.sock.recv(4096)
+        else:
+            self.data = None
+            self.sock.shutdown(1)
+            self.sock.close()
+            return self.data
+
+        self.sock.shutdown(1)
+        self.sock.close()
 
         # response holds the entire response, headers and body
         self.response = data
 
         # Split on \r\n to seperate headers and body
-        tmp = self.response.split(b"\r\n\r\n")
+        tmp = self.response.split(b"\r\n\r\n", maxsplit=1)
 
         # make that sweet sweet dict
-        self.data = {"headers": tmp[0].decode("ascii"), "body": tmp[1]}
+        try:
+            self.data = {"headers": tmp[0].decode("ascii"), "body": tmp[1]}
+        except IndexError:
+            self.data = None
+            return self.data
 
         # Get status code
         self.status = parse_value(self.data["headers"], "HTTP/1.1")
@@ -150,15 +183,15 @@ class SimpleRequest:
         else is received. Links not tested... only paths
         """
 
-        while self.redir:
+        while self.redir and self.data is not None:
             # Follow the redirect
             follow = parse_value(self.data["headers"], "Location:")
             if follow is not None:
-                parsed = parse_url(follow)
+                parsed = self.parse_url(follow)
 
                 # If host == None path was found not a new link
                 if parsed["host"] is None:
-                    follow = f"{parsed['resource']}"
+                    follow = f"{self.parsed['resource']}"
 
                     # Shenanigans for just a path being passed back
                     tmp = self.resource.split("/")
@@ -298,14 +331,18 @@ def thread_work(tasks, results):
     return True
 
 
-def thread_requests(requests):
+def thread_requests(requests, maxthreads=50):
     """
     This function takes a list of request objects and will spawn
     a thread for each object that will make the request outlined
     by each object.
 
     :param requests: A list of request objects that are ready to rock'n'roll
+    :param maxthreads: The maximum number of threads to spawn
     """
+
+    # How many threads!?
+    numThreads = min(len(requests), maxthreads)
 
     # Make a bunch of tasks (queue objects)
     tasks = Queue(maxsize=0)
@@ -314,7 +351,7 @@ def thread_requests(requests):
 
     # Make a list for results
     results = [None for i in range(len(requests))]
-    for idx in range(len(requests)):
+    for idx in range(len(numThreads)):
         # Full send the threads
         thread = (threading.Thread(group=None, target=thread_work, args=(tasks, results)))
         thread.start()
@@ -325,9 +362,142 @@ def thread_requests(requests):
     return results
 
 
-def crawl_work():
-    pass
+def crawl_work(tasks, results, scope):
+    """
+    This function will carry out the work for each thread spawned
+    from crawl(). Each thread will be in charge of parsing the HTML
+    to find the links for their respective pages
+
+    :param tasks: Queue objects for all the jobs task=(idx, requests[idx])
+    :param results: Place to store the results
+    :param scope: The scope to stay in
+    """
+
+    # Do the tasks
+    while not tasks.empty():
+        task = tasks.get(timeout=5)
+        # Shenanigans because web is dumb
+        while True:
+            try:
+                task[1].render()
+                task[1].send()
+                if task[1].data is None:
+                    continue
+                task[1].redirects()
+                if task[1].data is None:
+                    continue
+            except (ConnectionResetError, socket.gaierror, TimeoutError, queue.Empty):
+                continue
+            else:
+                break
+
+        # Save the body
+        body = task[1].data["body"]
+
+        # Try decoding... reeeeeeeeee things break
+        try:
+            body.decode("utf-8")
+        except UnicodeDecodeError:
+            body.decode("latin-1")
+
+        # Make soup for the thread
+        soup = bs4.BeautifulSoup(body, "html.parser")
+
+        tags = soup.find_all("a")
+
+        links = []
+        for tag in tags:
+            try:
+                links.append(tag["href"])
+            except KeyError:
+                pass
+
+        base = re.escape(scope)
+        # Matches relative or absolute RIT links
+        valid = re.compile("^(https?://[a-zA-Z0-9_\-\.]*" + base + ".*/?|)(/.*)")
+        links_in_scope = []
+        # for link in links:
+        #     if valid.search(link):
+        #         links_in_scope.append(link)
+        links_in_scope = [valid.search(i).group() for i in links
+                            if valid.match(i)]
+        results[task[0]] = links_in_scope
+        print(f"Done with page {task[0]}")
+        tasks.task_done()
+    return True
 
 
-def crawl(scope, depth=5):
-    pass
+def crawl(init_req, links, scope, depth=4, maxthreads=50):
+    """
+    This function crawls through a site confined by the "scope". It
+    takes a list of links from an initial request that should be
+    made from the originating script. This is because sending one
+    request is no problem. After receiving the params, it will
+    build the requests and spawn threads to crawl each page for links.
+    It will then save all links that are visited and make sure duplicates
+    are not saved.
+
+    :param init_req: request object from the first request
+    :param links: The initial list of links from the first request
+    :param scope: The scope of the crawler EX. (rit.edu)
+    :param depth: How deep to crawl, defaults to 4
+    :param maxthreads: The max threads to spawn, defaults to 50
+    """
+
+    # For saving searched links
+    searched = []
+
+    # Since this function takes the list of links from the first page
+    # We will do (depth-1), since we are starting at zero, to ensure
+    # we go the required depth. We are effectively starting at a depth
+    # of one
+
+    for x in range(depth):
+        print(f"[+] starting crawl to depth {x+2}...")
+
+        # Prune the initial list of links for scope valid links
+        # also parse the link and create a request object
+        requests = []
+        for link in links:
+            if (link):
+                # Check scope and path
+                if (scope in link) or (link[0] == "/"):
+                    urlDict = init_req.parse_url(link)
+                    requests.append(SimpleRequest(urlDict["host"], resource=urlDict["resource"], port=443, https=urlDict["https"]))
+
+        numThreads = min(len(requests), maxthreads)
+        # Make a bunch of tasks (queue objects)
+        tasks = Queue(maxsize=0)
+        for idx in range(len(requests)):
+            tasks.put((idx, requests[idx]))
+
+        # Make a list for results
+        results = [None for i in range(len(requests))]
+        for idx in range(numThreads):
+            # Full send the threads
+            thread = (threading.Thread(group=None, target=crawl_work, args=(tasks, results, scope)))
+            thread.start()
+
+        # That good good thread stuff
+        tasks.join()
+        print(len(results))
+
+        # Updates
+        print(f"[+] finished searched {len(requests)} links...")
+
+        # Save searched links so we don't repeat
+        searched.extend(links)
+
+        # Save the new links
+        links = []
+        for result in results:
+            for link in result:
+                if (link not in links) and (link not in searched):
+                    links.append(link)
+
+        print(links)
+        print(len(links))
+        print(f"[+] beginning search of {len(links)} links...")
+        # Loop with new "links" list
+
+    return searched
